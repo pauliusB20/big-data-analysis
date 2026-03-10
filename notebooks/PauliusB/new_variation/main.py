f3556ee86e80b8c3f2526754b7075d9d7ba1842c
Paulius Balciunas <studvuit.pauliusbalciunas@gmail.com>
2026-03-10 11:11:01 +0100

    [FIX] notebooks: optimized code for task 1 and task 2

--- notebooks/PauliusB/new_variation/main.py
+++ notebooks/PauliusB/new_variation/main.py
@@ -1,8 +1,10 @@
 from multiprocessing import Pool
 from datetime import datetime
 from collections.abc import Iterable
+from collections import defaultdict
+from tqdm import tqdm
 import csv
-import re
+import os
 
 # TODO: 
 # Optimize
@@ -11,21 +13,19 @@ FILE_CSV = "aisdk-2025-02-28.csv"
 
 # WARNING: This is max value. If you increase, than more memory is used exponentially
 CHUNK_SIZE = 1000
-
-MMSI_REGEX = re.compile(r"^[2-7][0-9]{8}$")
 WORKERS = 3
 
-def _is_mmsi_valid(mmsi: str) -> bool:
-    return bool(MMSI_REGEX.match(mmsi))
 
-def _get_valid_data_chunk(file_path: str, chunk_size: int) -> Iterable[dict]:    
+def _is_mmsi_valid(mmsi: str) -> bool:
+    return 2 <= int(mmsi[0]) <= 7 and len(mmsi) == 9
+
+def _read_chunks(file_path: str, chunk_size: int) -> Iterable[dict]:    
     with open(file_path) as csv_file:
         reader = csv.DictReader(csv_file)
         chunk = []
         for row in reader:            
-            # Optimization: check during reading mmsi
             if _is_mmsi_valid(row["MMSI"]):
-                chunk.append(row)     
+                chunk.append({"mmsi" : row["MMSI"], "Timestamp" : row["# Timestamp"]})     
                        
             if len(chunk) >= chunk_size:
                 yield chunk
@@ -34,17 +34,26 @@ def _get_valid_data_chunk(file_path: str, chunk_size: int) -> Iterable[dict]:
     if chunk:
         yield chunk
 
-def process_ship_chunk(chunk: list[dict]) -> tuple[int, bool]:
-    def _get_map_reduced_chunk(chunk: list[dict]):
-        # grouping
-        mmsi_mapped = {}   
-        for row in chunk:
-            mmsi_mapped.setdefault(row["MMSI"], []).append(row)
-        
-        return mmsi_mapped
-            
-    result = _get_map_reduced_chunk(chunk)
-    return result 
+def _get_mapped_chunk(chunk: list[dict]):
+    # grouping
+    ships = defaultdict(list)
+     
+    for row in chunk:
+        ships[row["mmsi"]].append(row["Timestamp"])    
+    
+    for mmsi in ships:
+        ships[mmsi].sort()    
+    
+    return ships
+
+def process_ship_chunk(chunk: list[dict]) -> tuple[int, dict]:            
+    result = _get_mapped_chunk(chunk)
+    pid = os.getpid()
+    
+    # TODO:
+    # add code for anomaly finding
+    
+    return pid, result 
     
     # end_time = datetime.now()
     # execution_time = (end_time - start_time).seconds
@@ -53,21 +62,27 @@ def process_ship_chunk(chunk: list[dict]) -> tuple[int, bool]:
 
 if __name__ == "__main__":
     
-    reader_cursor = _get_valid_data_chunk(FILE_CSV, CHUNK_SIZE)
-    global_vessels = {}
+    reader_cursor = _read_chunks(FILE_CSV, CHUNK_SIZE)
+    global_states = defaultdict(list)
     start_time = datetime.now()
     print("Starting SHIP Data tool")
     
     with Pool(processes=WORKERS) as pool:
-        for mapped in pool.imap_unordered(process_ship_chunk, reader_cursor, chunksize=1):
-            # merge to global
-            for mmsi, records in mapped.items():
-                global_vessels.setdefault(mmsi, []).extend(records)
+        for pid, mapped in tqdm(
+            pool.imap_unordered(process_ship_chunk, reader_cursor, chunksize=1),
+            desc="Processing chunk"
+        ):
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"Task = {pid} Finished proccesing at {timestamp}")
             
-        for mmsi in global_vessels:
-            global_vessels[mmsi] = sorted(global_vessels[mmsi], key=lambda x: x["# Timestamp"])
+            # merge to global
+            for mmsi, timestamps in mapped.items():
+                global_states[mmsi].extend(timestamps)
+            
+    for mmsi in global_states:
+        global_states[mmsi].sort()
         
-    print(f"Proccesed ships {len(global_vessels.keys())}")
+    print(f"Proccesed ships {len(global_states)}")
     end_time = datetime.now()
     execution_difference = (end_time - start_time).seconds
     print(f"Execution time: {execution_difference} s")
