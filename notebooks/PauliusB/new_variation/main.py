e83aa23b677cee712ffbba5ff4df1c287cc04a2e
Paulius Balciunas <studvuit.pauliusbalciunas@gmail.com>
2026-03-17 01:49:53 +0100

    [FIX] notebooks: optimized paralell code

--- notebooks/PauliusB/new_variation/main.py
+++ notebooks/PauliusB/new_variation/main.py
@@ -1,7 +1,7 @@
 from multiprocessing import Pool
 from datetime import datetime
 from collections.abc import Iterable
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from tqdm import tqdm
 import csv
 import os
@@ -14,10 +14,18 @@ FILE_CSV = "aisdk-2025-02-28.csv"
 # WARNING: Max chunk size = 1000
 CHUNK_SIZE = 1000
 
-# WARNING: Max 7 workers
+# After which count, display log message
+FINISHED_TASKS = 1000
+
+# Max tasks workers can proccess
+TASKS_PER_WORKER = 25
+
 # NOTE: check how many cores your CPU has. If 8 cores, max 8 proccesses
+# WARNING: tested Max 7 workers for 8 cores and no memory overflow
 WORKERS = 7
 
+# Quick Classes
+ShipRow = namedtuple("ShipRow", ["mmsi", "timestamp"])
 
 def _is_mmsi_valid(mmsi: str) -> bool:
     return (
@@ -27,13 +35,20 @@ def _is_mmsi_valid(mmsi: str) -> bool:
         and len(set(mmsi)) > 1
     )
 
+
 def _read_chunks(file_path: str, chunk_size: int) -> Iterable[dict]:    
     with open(file_path) as csv_file:
-        reader = csv.DictReader(csv_file)
+        reader = csv.reader(csv_file)
+        headline = next(reader)
+        
+        # columns to select
+        mmsi_idx = headline.index("MMSI")
+        timestamp_idx = headline.index("# Timestamp")
+        
         chunk = []
         for row in reader:            
-            if _is_mmsi_valid(row["MMSI"]):
-                chunk.append({"mmsi" : row["MMSI"], "Timestamp" : row["# Timestamp"]})     
+            if _is_mmsi_valid(row[mmsi_idx]):
+                chunk.append(ShipRow(row[mmsi_idx], row[timestamp_idx]))  
                        
             if len(chunk) >= chunk_size:
                 yield chunk
@@ -47,7 +62,7 @@ def _get_mapped_chunk(chunk: list[dict]):
     ships = defaultdict(list)
      
     for row in chunk:
-        ships[row["mmsi"]].append(row["Timestamp"])    
+        ships[row.mmsi].append(row.timestamp)    
     
     for mmsi in ships:
         ships[mmsi].sort()    
@@ -75,13 +90,17 @@ if __name__ == "__main__":
     start_time = datetime.now()
     print("Starting SHIP Data tool")
     
+    task_counts = defaultdict(list)
+    
     with Pool(processes=WORKERS) as pool:
         for pid, mapped in tqdm(
-            pool.imap_unordered(process_ship_chunk, reader_cursor, chunksize=1),
+            pool.imap_unordered(process_ship_chunk, reader_cursor, chunksize=TASKS_PER_WORKER),
             desc="Processing chunk"
         ):
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"Task = {pid} Finished proccesing at {timestamp}")
+            task_counts[pid] = task_counts.get(pid, 0) + 1
+            if task_counts[pid] % FINISHED_TASKS == 0:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"Proccess = {pid} Finished proccesing {FINISHED_TASKS} at {timestamp}")                
             
             # merge to global
             for mmsi, timestamps in mapped.items():
