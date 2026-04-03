ca361f8408f89f001000215a208e7a35878efe34
Paulius Balciunas <studvuit.pauliusbalciunas@gmail.com>
2026-04-03 03:27:02 +0200

    [FIX] added fixes to parser data writing, helper methods, examples and initial doc

--- task_1/config.py
+++ task_1/config.py
@@ -5,12 +5,21 @@ class Config:
     
     """
     
+    # Config data for AIS Data parser
     CHUNK_SIZE = 1000
     LOG_EVERY = 10000
     TAKS_PER_WORKER = 10
     WORKERS = 6
     DB_NAME = "AIS_DB.db"
     DB_TABLE = "AIS_TABLE"
-    CSV_FILE_SOURCE = ["aisdk-2025-02-28.csv"]
+    CSV_FILE_SOURCE = [
+        "aisdk-2025-02-28.csv",
+        "aisdk-2025-03-01.csv"
+    ]
     
-    # TODO: add additional config params here for other anomalies
+    # TODO: add additional config params here for other anomalies if needed
+    # For importing and using config.py, example:
+    # from config import Config
+    # 
+    # config = Config()
+    # print(config.CHUNK_SIZE)
--- task_1/main.py
+++ task_1/main.py
@@ -4,34 +4,96 @@ from collections.abc import Iterable
 from collections import defaultdict
 from dataclasses import astuple
 from datetime import datetime
-from parser import run_parser
+from parser import run_ais_parsers
 # from haversine import haversine, Unit
 from collections import defaultdict
 from config import Config
+from helper import DBHelper
 from tqdm import tqdm
 import numpy as np
 import csv
 import os            
 
-# TODO
-def _retrieve_records(CHUNK: int) -> list:
+
+
+def _run_anomaly_a_analysis(config: Config) -> None:
+    """
+    Anomaly A analysis
+    """
+    
+    # TODO: Anomaly A detection
+    
     pass
+
+def _run_anomaly_b_analysis(config: Config) -> None:
+    """
+    Anomaly B analysis
+    """
     
-if __name__ == "__main__":
+    # TODO: Anomaly B detection
     
+    pass
+
+
+def _run_anomaly_c_analysis(config: Config) -> None:
+    """
+    Anomaly C analysis
+    """
+    
+    # TODO: Anomaly C detection
+    
+    pass
+
+
+def _run_anomaly_d_analysis(config: Config) -> None:
+    """
+    Anomaly D analysis
+    """
+    
+    # TODO: Anomaly D detection
+    
+    pass
+
+
+
+
+
+def run_anomaly_analysis(config: Config) -> None:
+
+    """
+    Anomaly analysis
+    """
+    
+    _run_anomaly_a_analysis(config)
+    _run_anomaly_b_analysis(config)
+    _run_anomaly_c_analysis(config)
+    _run_anomaly_d_analysis(config)
+
+    
+if __name__ == "__main__":    
     
     config = Config()
     
     print("-----STARTING AIS DATA ANALYZER-----")
     
     start_time = datetime.now()
-    run_parser(config)
+    start_time_str = start_time.strftime("%Y-%m-%d %H:%M:%S")
+    print(
+        f"Started run time at: {start_time_str}"
+        "-------------------"
+    )
     
-    # Anomaly detection goes here 
+    run_ais_parsers(config)
+    
+    run_anomaly_analysis()
     
     
     end_time = datetime.now()
+    end_time_str = end_time.strftime("%Y-%m-%d %H:%M:%S")
     execution_time = (end_time - start_time).seconds
     print(f"Total execution time: {execution_time}s")
-    
+    print(
+        "------------------\n"
+        f"Finished runtime at {end_time_str}"
+    )
     print("DONE")
